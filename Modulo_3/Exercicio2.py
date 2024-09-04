5a3057ffc3ae7ae668f3cec1382ebcb41a450802
Beatriz de M. Porto <biamattos_porto@hotmail.com>
2024-09-04 19:52:19 -0300

    Update and rename ex2.py to Exercicio2.py

---
+++ Modulo_3/Exercicio2.py
@@ -0,0 +1,9 @@
+'''Receba um número inteiro na entrada e imprima "Fizz"
+se o número for divisível por 3. Caso contrário, imprima o mesmo número que foi dado na entrada.'''
+
+numero = int(input("Digite um número: "))
+
+if numero % 3 == 0:
+    print("Fizz")
+else:
+    print(numero)
--- Modulo_3/ex2.py
+++
@@ -1,6 +0,0 @@
-numero = int(input("Digite um número: "))
-
-if numero % 3 == 0:
-    print("Fizz")
-else:
-    print(numero)
