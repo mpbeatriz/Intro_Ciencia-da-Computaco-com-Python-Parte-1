81dce38aad1ae3ca38765248ab0ac014b55b50e8
Beatriz de M. Porto <biamattos_porto@hotmail.com>
2024-09-04 19:47:35 -0300

    Create Exercicio1.py

---
+++ Modulo_3/Exercicio1.py
@@ -0,0 +1,9 @@
+'''Receba um número inteiro na entrada e imprima 'par', quando
+o número for par, ou 'ímpar' quando o número for ímpar.'''
+
+numero = int(input("Digite seu número: "))
+
+if numero % 2 ==0:
+    print("Par.")
+else:
+    print("Ímpar.")
