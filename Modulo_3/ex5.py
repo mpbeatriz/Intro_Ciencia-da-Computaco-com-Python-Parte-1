ead007e3768c1c8438a6f96fc117f90c57ac95e3
Beatriz de M. Porto <biamattos_porto@hotmail.com>
2024-09-04 19:51:25 -0300

    Add files via upload

---
+++ Modulo_3/ex5.py
@@ -0,0 +1,8 @@
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Digite um número: "))
+numero3 = int(input("Digite um número: "))
+
+if numero1 <= numero2 <= numero3:
+    print("Crescente.")
+else:
+    print("Não está em ordem crescente.")
